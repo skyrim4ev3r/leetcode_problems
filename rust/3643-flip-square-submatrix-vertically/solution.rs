76454642c560d9e5b3b4e6976ecf4d3d9c566ef6
skyrim4ev3r <koosha.skyrim1991@gmail.com>
2025-09-06 01:54:31 +0330

    3643-flip-square-submatrix-vertically

---
+++ rust/3643-flip-square-submatrix-vertically/solution.rs
@@ -0,0 +1,7 @@
+//leetcode.com/problems/flip-square-submatrix-vertically
+impl Solution {
+    pub fn reverse_submatrix(mut grid: Vec<Vec<i32>>, x: i32, y: i32, k: i32) -> Vec<Vec<i32>> {
+        (0..k).clone().for_each(|j| (0..k/2).clone().for_each(|i| (grid[(x+i) as usize][(y+j)  as usize], grid[(x+k-i-1) as usize][(y+j) as usize]) = (grid[(x+k-i-1) as usize][(y+j) as usize],grid[(x+i) as usize][(y+j)  as usize])));                
+        grid
+    }
+}
