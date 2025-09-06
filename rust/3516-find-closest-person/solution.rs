78c23169da8c84b2934c1b4418f77339b2e43179
skyrim4ev3r <koosha.skyrim1991@gmail.com>
2025-09-06 22:54:43 +0330

    deleted files

--- rust/3516-find-closest-person/solution.rs
+++
@@ -1,6 +0,0 @@
-//https://leetcode.com/problems/find-closest-person/
-impl Solution {
-    pub fn find_closest(x: i32, y: i32, z: i32) -> i32 { 
-        ((z-x).abs() < (y-z).abs()) as i32 + 2 *(((z-x).abs() > (y-z).abs()) as i32)
-    }
-}
