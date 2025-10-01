a0d83718e2dc37e02082d4a84cdafadae5cb1056
skyrim4ev3r <koosha.skyrim1991@gmail.com>
2025-10-01 14:06:46 +0330

    added-solutions

---
+++ 2-medium/02390-removing-stars-from-a-string/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        vector<char> stack{};
+        for (auto ch: s) {
+            if (ch == '*' && !stack.empty()) {
+                stack.pop_back();
+            } else {
+                stack.push_back(ch);
+            }
+        }
+
+        string res {};
+        res.insert(res.end(), stack.begin(), stack.end());
+
+        return res;
+    }
+};
